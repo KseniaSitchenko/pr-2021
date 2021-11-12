58d783f106ee24b00c99410cea31206cd42b658f
KseniaSitchenko <sitchenko.ksenia@gmail.com>
2021-11-12 03:57:10 +0300

    LW2 code

---
+++ LW №2/Poke/src/Tangrowth.java
@@ -0,0 +1,10 @@
+import ru.ifmo.se.pokemon.*;
+
+public class Tangrowth extends Tangela {
+    public Tangrowth(String name, int level) {
+        super(name, level);
+        setStats(100,100, 125, 110, 50, 50);
+        setType(Type.GRASS);
+        setMove(new Facade(), new Confide(), new Swagger(), new CottonSpore());
+    }
+}
